d5b14e1c97eb3cdb991a58b10755422f61a5658e
wagnertrindades <wagner.sanseverin@resultadosdigitais.com.br>
2016-08-23 21:16:37 -0300

    Fix geocode with decode in UTF-8

--- api_maps_foursquare/geocode.py
+++ api_maps_foursquare/geocode.py
@@ -4,11 +4,12 @@ import json
 def getGeocodeLocation(inputString):
     # Use Google Maps to convert a location into Latitute/Longitute coordinates
     # FORMAT: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
-    google_api_key = "PASTE_YOUR_KEY_HERE"
+    google_api_key = "AIzaSyCz1CbAABQzWRLOe9et5NuditEUKridyoA"
     locationString = inputString.replace(" ", "+")
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
     h = httplib2.Http()
-    result = json.loads(h.request(url,'GET')[1])
+    decoded_request = h.request(url,'GET')[1].decode('UTF-8')
+    result = json.loads(decoded_request)
     latitude = result['results'][0]['geometry']['location']['lat']
     longitude = result['results'][0]['geometry']['location']['lng']
     return (latitude,longitude)
